482be932c223b334f763cac550dfc3fa8aaba441
Vivienne Lu <viviennelu@MacBook-Pro.local>
2022-03-06 19:28:22 -0600

    New Home Page

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ import Card from "../components/Card";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <Layout>
-      <h1>Home Page Here</h1>
-      <Card
-        name="Test Event Card"
-        location="at a location"
-        time="at time x:xx pm"
-      ></Card>
-    </Layout>
-  );
+    return ( <
+        Layout >
+        <
+        h1 > New Home Page < /h1> <
+        Card name = "Test Event Card"
+        location = "at a location"
+        time = "at time x:xx pm" >
+        < /Card> <
+        /Layout>
+    );
 }
