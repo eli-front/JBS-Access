021bf8a6c20b84c30b5abaf7120c9488eb0f501d
Davis Engler <davispe33@Davis-Englers-MacBook.local>
2022-03-07 13:21:39 -0600

    Worked on navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,8 +5,17 @@ import styles from "../styles/Navbar.module.css"
 export default function Navbar() {
   return (
     <>
-      <div className={styles.navbar}>
-        <h1>JBS Access</h1>
+      <div className={styles.name}>
+        <h1><Link href="/"><a>JBS Access</a></Link></h1>
+      </div>
+      <div>
+        page
+      </div>
+      <div>
+        page
+      </div>
+      <div>
+        about
       </div>
     </>
   );
