07b71e476464715fb48675a9eafbcb806931f025
Eli Front <elifront23@gmail.com>
2022-03-13 15:29:13 -0500

    base counselor contact

--- pages/contact-counselor.js
+++ pages/contact-counselor.js
@@ -1,9 +1,48 @@
 import Layout from "../components/Layout";
+import styles from "../styles/ContactCounselor.module.css";
+import { useState } from "react";
 
 export default function ContactCounselor() {
+  let [name, setName] = useState("");
+  let [location, setLocation] = useState("");
+
+  let [messageSent, setMessageSent] = useState(false);
+
+  function sendEmail(e) {
+    e.preventDefault();
+    setMessageSent(true);
+  }
   return (
     <Layout>
-      <h1>help</h1>
+      <form id={styles.contact_form} onSubmit={sendEmail}>
+        <label>What&apos;s your name?</label>
+        <input
+          type="text"
+          id="name"
+          autoComplete="off"
+          required
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <label>Where are you?</label>
+        <input
+          type="text"
+          id="location"
+          autoComplete="off"
+          required
+          onChange={(e) => {
+            setLocation(e.target.value);
+          }}
+        />
+        <button>send a counselor</button>
+      </form>
+
+      {messageSent && (
+        <p id={styles.confirmation_message}>
+          Your message was sent. The conselor will be with you ASAP.
+        </p>
+      )}
     </Layout>
   );
 }
