7d3bdfb739907294f4329555791b024fa4bdde97
Davis Engler <davispe33@Davis-Englers-MacBook.local>
2022-03-12 22:01:07 -0600

    Mostly finished up the navbar, at least for mobile view. Currently have placeholder links

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,17 +5,11 @@ import styles from "../styles/Navbar.module.css"
 export default function Navbar() {
   return (
     <>
-      <div className={styles.name}>
-        <h1><Link href="/"><a>JBS Access</a></Link></h1>
-      </div>
-      <div>
-        page
-      </div>
-      <div>
-        page
-      </div>
-      <div>
-        about
+      <div className={styles.navbar}>
+        <Link href="/"><a className={styles.navbarname}>JBS Access</a></Link>
+        <Link href="https://www.youtube.com"><a className={styles.navbaritem}>Page</a></Link>
+        <Link href="https://www.twitter.com"><a className={styles.navbaritem}>Page</a></Link>
+        <Link href="https://www.jburroughs.org"><a className={styles.navbaritem}>About</a></Link>
       </div>
     </>
   );
