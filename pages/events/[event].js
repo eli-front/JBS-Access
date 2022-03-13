e9839b7c9fe2a82edee74df5b948c5f293fd6a42
Eli Front <elifront23@gmail.com>
2022-03-13 13:27:00 -0500

    load dummy event data

--- pages/events/[event].js
+++ pages/events/[event].js
@@ -1,33 +1,30 @@
 import Layout from "../../components/Layout";
 import { useRouter } from "next/router";
 import Card from "../../components/Card";
-//import {Event} from "../../lib/Event.js"
+import Event from "../../lib/Event";
+
 export default function EventPage() {
   const router = useRouter();
   const { event } = router.query;
-  const events = [new Event('Boys Varsity Lacrosse vs CBC','4:30 pm', 'Leland Field'),
-  new Event('Varsity Water Polo vs Parkway Central', '4:30 pm', 'Natatorium'),
-  new Event('Girls JV Soccer vs Lindbergh', '5:15 pm', 'Beckman Field')];
+  const events = [
+    new Event("Boys Varsity Lacrosse vs CBC", "4:30 pm", "Leland Field"),
+    new Event("Varsity Water Polo vs Parkway Central", "4:30 pm", "Natatorium"),
+    new Event("Girls JV Soccer vs Lindbergh", "5:15 pm", "Beckman Field"),
+  ];
 
   return (
     <Layout>
       <h1>{event}</h1>
-      {events.map((events, index) => {
-
-        return <Card key={index} name={eventName} location={loc} time={startTime}>
-          
-          </Card>
+      {events.map((event, index) => {
+        return (
+          <Card
+            key={index}
+            name={event.eventName}
+            location={event.loc}
+            time={event.startTime}
+          ></Card>
+        );
       })}
     </Layout>
   );
 }
-
-class Event {
-  constructor(eventName, startTime, loc) {
-      this.eventName = eventName;
-      this.startTime = startTime;
-      this.loc = loc;
-
-  }
-};
-
