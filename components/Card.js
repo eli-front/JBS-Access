660228f5bb89e1691232c17ec87ad24e0bea0293
BigDanielGreene <2024.dgreene@jburroughs.org>
2022-03-14 22:37:12 -0500

    worked some more on the cards. They might be done, unless anyone has ideas to change/modify/improve it

--- components/Card.js
+++ components/Card.js
@@ -1,13 +1,19 @@
-import styles from "../styles/Card.module.css";
+import styles from "../styles/Card.module.css"; 
+import locationMarker from "../public/icons/locationMarker.png";
+import Image from "next/image";
 
 export default function Card({ name, location, time }) {
   return (
     <>
       <p className={styles.button}>
+          <div className={styles.imageWrapper}>
+      <Image src={locationMarker} width={60} height={60} ></Image>
+          </div>
+        
         <p id={styles.name}>{name}</p>
         <p id={styles.location}>{location}</p>
         <p id={styles.time}>{time}</p>
-        <img src=""></img>
+        
       </p>
     </>
   );
