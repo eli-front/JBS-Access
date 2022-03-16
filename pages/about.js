30b5ea681aa216d9fdef075421e8a17d952204b2
Eli Front <elifront23@gmail.com>
2022-03-16 14:55:45 -0500

    about page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,25 @@
-import Image from "next/image";
-import Link from "next/link";
-import styles from "../styles/Navbar.module.css"
 import Layout from "../components/Layout";
+import styles from "../styles/About.module.css";
+
 export default function About() {
-    return (
-       <Layout></Layout>
-    );
+  return (
+    <Layout>
+      <div id={styles.about_header}>
+        <h1>About JBS Access</h1>
+      </div>
+
+      <p id={styles.about_body}>
+        The John Burroughs community is very fortunate to have access to a
+        beautiful campus and resources for students. On campus there are
+        elevators, ramps, and gender-neutral bathrooms. However, despite having
+        these resources, it can be challenging for our visitors and people
+        within our community to become familiar with them. At the forefront of
+        our website is accessibility; through thoughtful design we created a
+        navigation system that highlights routes suitable for wheelchairs,
+        mobility aids, and strollers- door types, stairs, and entrance width
+        were all taken into account. We are so grateful to have you as a part of
+        our community- please explore the site and our campus!
+      </p>
+    </Layout>
+  );
 }
